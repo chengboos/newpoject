83a2069d156b0a28c8f81859ba9214a68158b900
chengboos <xiecheng0822@163.com>
2020-09-04 17:49:45 +0800

    2ci22

---
+++ src/cn/xc/Test2.java
@@ -0,0 +1,13 @@
+package cn.xc;
+
+public class Test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hub222");
+	}
+
+}
